304254232d3dc29b6fe8c9e510355cb085da037d
ayushh-gautam <ayushgautam896@gmail.com>
2024-04-12 10:36:02 +0545

    import changes

--- lib/multi_bloc_provider.dart
+++ lib/multi_bloc_provider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'service_locator.dart';
 import 'src/controller/login/login_bloc.dart';
 
 class MultiBlocProviderClass extends StatelessWidget {
