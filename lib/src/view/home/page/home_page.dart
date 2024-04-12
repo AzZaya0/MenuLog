19b0b39fbfa16f04fadff14ddbc3c7b97597df14
ayushh-gautam <ayushgautam896@gmail.com>
2024-04-12 18:00:45 +0545

    home page modified as well as menu page is also added

--- lib/src/view/home/page/home_page.dart
+++ lib/src/view/home/page/home_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:menu_log/commons/controls/custom_text.dart';
+import 'package:menu_log/src/view/menu/menu.dart';
+import 'package:menu_log/utils/extension.dart';
 
 import '../../../../commons/controls/custom_button.dart';
 import '../../../../utils/app_color.dart';
@@ -13,26 +15,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(200),
-              child: Image.network(userCredential.user?.photoURL ?? ''),
-            ),
-            CustomText(text: userCredential.user?.displayName ?? ''),
-            CustomButton(
-              text: 'Google logout',
-              textColor: AppColor.white,
-              onTap: () {
-                context.read<LoginBloc>().add(OnGoogleLogout());
-              },
-            )
-          ],
+        body: SafeArea(
+      child: CustomScrollView(slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(300),
+                  child: Image.network(
+                    userCredential.user?.photoURL ??
+                        'https://i.pinimg.com/736x/01/de/87/01de8790415df8a899fb0420458c4a9c.jpg',
+                    height: 40,
+                    width: 40,
+                  ),
+                ),
+                CustomText(text: userCredential.user?.displayName ?? ''),
+              ]),
         ),
-      ),
-    );
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 0.90,
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+          ),
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return const MenuPage();
+                }));
+              },
+              child: SizedBox(
+                child: Image.network(
+                  'https://i.pinimg.com/736x/01/de/87/01de8790415df8a899fb0420458c4a9c.jpg',
+                ),
+              ).addMargin(const EdgeInsets.only(left: 10, right: 10)),
+            );
+          }),
+        ),
+        SliverToBoxAdapter(
+          child: CustomButton(
+            text: 'Google logout',
+            textColor: AppColor.white,
+            onTap: () {
+              context.read<LoginBloc>().add(OnGoogleLogout());
+            },
+          ),
+        )
+      ]),
+    ));
   }
 }
